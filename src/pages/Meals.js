fc7cc4831683c0788811927f3eae8df8ac5ee311
jorgeuser <jorge.araujo.kgb@gmail.com>
2022-12-08 11:04:04 -0300

    Enviando o requisito 16

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
 import '../styles/header.css';
 import Header from '../components/Header';
+import Footer from '../components/Footer';
 import {
   firstTwelveFoods, allFoodCategories, firstTwelveFoodCategories,
 } from '../services/Apis';
@@ -28,43 +29,46 @@ function Meals() {
     setTwelveFoods(await firstTwelveFoodCategories(name));
   };
   return (
-    <div className="container_meals">
-      <Header />
-      <div>
-        {categoryName.slice(0, FIVE).map(({ strCategory }) => (
+    <>
+      <div className="container_meals">
+        <Header />
+        <div>
+          {categoryName.slice(0, FIVE).map(({ strCategory }) => (
+            <button
+              data-testid={ `${strCategory}-category-filter` }
+              type="button"
+              name={ strCategory }
+              key={ strCategory }
+              onClick={ foodButton }
+            >
+              {strCategory}
+            </button>
+          ))}
           <button
-            data-testid={ `${strCategory}-category-filter` }
+            data-testid="All-category-filter"
+            onClick={ async ({ target: { name } }) => {
+              if (name === 'all') return setTwelveFoods(await firstTwelveFoods());
+              setTwelveFoods(await firstTwelveFoodCategories(name));
+            } }
             type="button"
-            name={ strCategory }
-            key={ strCategory }
-            onClick={ foodButton }
+            name="all"
           >
-            {strCategory}
+            All
           </button>
-        ))}
-        <button
-          data-testid="All-category-filter"
-          onClick={ async ({ target: { name } }) => {
-            if (name === 'all') return setTwelveFoods(await firstTwelveFoods());
-            setTwelveFoods(await firstTwelveFoodCategories(name));
-          } }
-          type="button"
-          name="all"
-        >
-          All
-        </button>
-      </div>
-      {twelveFoods.slice(0, TWELVE).map(({ strMeal, strMealThumb, idMeal }, index) => (
-        <div data-testid={ `${index}-recipe-card` } key={ idMeal }>
-          <img
-            src={ strMealThumb }
-            alt={ strMeal }
-            data-testid={ `${index}-card-img` }
-          />
-          <p data-testid={ `${index}-card-name` }>{strMeal}</p>
         </div>
-      ))}
-    </div>
+        {twelveFoods.slice(0, TWELVE).map(({ strMeal, strMealThumb, idMeal }, index) => (
+          <div data-testid={ `${index}-recipe-card` } key={ idMeal }>
+            <img
+              src={ strMealThumb }
+              alt={ strMeal }
+              data-testid={ `${index}-card-img` }
+            />
+            <p data-testid={ `${index}-card-name` }>{strMeal}</p>
+          </div>
+        ))}
+      </div>
+      <Footer data-testid="footer" />
+    </>
   );
 }
 
