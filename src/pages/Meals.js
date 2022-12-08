88cf2581da5d4b43f571d14b0bac9588289e6010
joabe <joabe9999pereira@gmail.com>
2022-12-07 23:23:27 -0300

    requisito 20 && 21

--- src/pages/Meals.js
+++ src/pages/Meals.js
@@ -1,16 +1,21 @@
-import React, { useContext, useState, useEffect } from 'react'; /* { useState }  */
+import React, { useContext, useState, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
 import '../styles/header.css';
 import Header from '../components/Header';
-import { firstTwelveFoods } from '../services/Apis';
+import {
+  firstTwelveFoods, allFoodCategories, firstTwelveFoodCategories,
+} from '../services/Apis';
 
 function Meals() {
   const [twelveFoods, setTwelveFoods] = useState([]);
+  const [categoryName, setCategoryName] = useState([]);
+  const { setTitle } = useContext(RecipesContext);
+  const TWELVE = 12;
+  const FIVE = 5;
   const setFoods = async () => {
     setTwelveFoods(await firstTwelveFoods());
+    setCategoryName(await allFoodCategories());
   };
-  const { setTitle } = useContext(RecipesContext);
-
   useEffect(() => {
     setTitle('Meals');
   }, []);
@@ -18,10 +23,37 @@ function Meals() {
   useEffect(() => {
     setFoods();
   }, []);
-  const TWELVE = 12;
+  const foodButton = async ({ target: { name } }) => {
+    if (name === 'all') return setTwelveFoods(await firstTwelveFoods());
+    setTwelveFoods(await firstTwelveFoodCategories(name));
+  };
   return (
     <div className="container_meals">
       <Header />
+      <div>
+        {categoryName.slice(0, FIVE).map(({ strCategory }) => (
+          <button
+            data-testid={ `${strCategory}-category-filter` }
+            type="button"
+            name={ strCategory }
+            key={ strCategory }
+            onClick={ foodButton }
+          >
+            {strCategory}
+          </button>
+        ))}
+        <button
+          data-testid="All-category-filter"
+          onClick={ async ({ target: { name } }) => {
+            if (name === 'all') return setTwelveFoods(await firstTwelveFoods());
+            setTwelveFoods(await firstTwelveFoodCategories(name));
+          } }
+          type="button"
+          name="all"
+        >
+          All
+        </button>
+      </div>
       {twelveFoods.slice(0, TWELVE).map(({ strMeal, strMealThumb, idMeal }, index) => (
         <div data-testid={ `${index}-recipe-card` } key={ idMeal }>
           <img
--- src/services/Apis.js
+++ src/services/Apis.js
@@ -49,3 +49,27 @@ export const firstTwelveDrinks = async () => {
   const data = await reponse.json();
   return data.drinks;
 };
+// todas as categorias de comidas
+export const allFoodCategories = async () => {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+  const data = await response.json();
+  return data.meals;
+};
+// todas as categorias de bebidas
+export const allDrinkCategories = async () => {
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+  const data = await response.json();
+  return data.drinks;
+};
+// categorias de bebidas filtradas por nome
+export const firstTwelveDrinkCategories = async (category) => {
+  const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`);
+  const data = await response.json();
+  return data.drinks;
+};
+// categorias de comidas filtradas por nome
+export const firstTwelveFoodCategories = async (category) => {
+  const response = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`);
+  const data = await response.json();
+  return data.meals;
+};
