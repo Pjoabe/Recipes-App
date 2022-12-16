3eb2a533d8575135912679f05aeaa8a52179bd53
Bete.mrt <bete.mrt@gmail.com>
2022-12-16 18:42:48 -0300

    Requisito 57 ok

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,8 @@
 import React, { useContext, useEffect } from 'react';
 import RecipesContext from '../context/RecipesContext';
-import '../styles/header.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import '../styles/profile.css';
 
 function Profile() {
   const { setTitle } = useContext(RecipesContext);
@@ -12,13 +12,44 @@ function Profile() {
   }, []);
 
   return (
-    <>
-      <div className="container_profile">
-        <Header />
-        <h1>Profile</h1>
+    <div className="container_profile">
+      <Header />
+      <div className="box_profile">
+        <label htmlFor="emailProfile">
+          Email:
+          <input
+            type="email"
+            name="emailProfile"
+            data-testid="profile-email"
+            placeholder="email@email.com"
+            title="Invalid email address"
+            required
+          />
+        </label>
+        <button
+          id="btnDoneProfile"
+          type="button"
+          data-testid="profile-done-btn"
+        >
+          Done Recipes
+        </button>
+        <button
+          id="btnFavoriteProfile"
+          type="button"
+          data-testid="profile-favorite-btn"
+        >
+          Favorite Recipes
+        </button>
+        <button
+          id="btnLogoutProfile"
+          type="button"
+          data-testid="profile-logout-btn"
+        >
+          Logout
+        </button>
       </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
